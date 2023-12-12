0eb5a312331281755732cecd6750071f67c8fe48
kadir-zeren <k4dzeren@gmail.com>
2023-12-12 18:03:01 +0300

    nestd function

--- arrow.js
+++ arrow.js
@@ -66,3 +66,41 @@ console.log("VOLUME:", volume(r, h).toExponential(5));
 
 //* ORNEK: Bir fonksiyon icerisinde baska fonksiyonlar cagrilabilir (Hesap Makinası)
 //**************************************************/
+const add = (n1, n2) => n1 + n2;
+const sub = (n1, n2) => n1 - n2;
+const mul = (n1, n2) => n1 * n2;
+const div = (n1, n2) => n1 / n2;
+
+const compute = (n1, n2, operator) => {
+  let result = 0;
+  switch (operator) {
+    case "+":
+      //   result = n1 + n2
+      result = add(n1, n2);
+      break;
+    case "-":
+      result = sub(n1, n2);
+      break;
+    case "x":
+    case "*":
+      result = mul(n1, n2);
+      break;
+    case "/":
+      result = div(n1, n2);
+      break;
+    default:
+      alert("Incorrect Operator");
+      result = "";
+      break;
+  }
+
+  return result;
+};
+
+const n1 = +prompt("First Number:");
+const operator = prompt("Operator:");
+const n2 = +prompt("Second Number:");
+
+console.log(`${n1}${operator}${n2}=${compute(n1, n2, operator)}`);
+
+//* ORNEK: Girilen sayinin faktoriyelini hesaplayan programı fonksiyonlar yardımıyla kodlayiniz.
